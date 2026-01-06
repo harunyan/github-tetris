dace89ad824e889e78629bdfef5dadd7792282d3
Morimoto haruhi <morimoto.haruhi@gmail.com>
2026-01-06 09:52:49 +0900

    chore: initial commit - scaffold tetris project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tetris from './tetris/Tetris'
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <h1>GitHub Tetris</h1>
+      <Tetris />
+    </div>
+  )
+}
